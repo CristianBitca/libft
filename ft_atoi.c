89af182d6cefd8c230a0ebe9a773a1db9a4dc24d
Cristian Bitca <cbitca@c1r3s2.42london.com>
2024-12-02 21:48:06 +0000

    atoi

---
+++ ft_atoi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbitca <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/02 20:35:19 by cbitca            #+#    #+#             */
+/*   Updated: 2024/12/02 20:35:21 by cbitca           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int    ft_atoi(const char *nptr)
+{
+    int    i;
+    int    sign;
+    int    result;
+
+    i = 0;
+    result = 0;
+    while (nptr[i] == ' ' || ('\t' <= nptr[i] && '\r' >= nptr[i]))
+        i++;
+    if (nptr[i] == '+')
+        sign = 1;
+    else if (nptr[i] == '-')
+        sign = -1;
+    i++;
+    while ('0' <= nptr[i] && '9' >= nptr[i])
+    {
+        result = result * 10 + nptr[i] - '0';
+        i++;
+    }
+    return (result * sign);
+}
