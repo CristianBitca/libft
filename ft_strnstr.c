47e00c2beb5447562842aeea62d9fe18c09c3f49
Cristian Bitca <cbitca@c1r3s2.42london.com>
2024-12-02 20:33:45 +0000

    update2

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -10,4 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+const char	*strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (*little == '\0')
+		return (big);
+	while (big[i] || i != len)
+	{
+		j = 0;
+		while (big[i + j] == little[j])
+		{
+			j++;
+			if (little[j] == '\0')
+				return (&big[i]);
+		}
+		i++;
+	}
+	return (0);
+}
