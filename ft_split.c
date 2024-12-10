c9bfd052d459524d3908a7c0708bfb3afd1da4c4
Cristian Bitca <cbitca@c1r7s3.42london.com>
2024-12-10 17:12:12 +0000

    makeup2

--- ft_split.c
+++ ft_split.c
@@ -13,36 +13,13 @@
 #include "libft.h"
 #include <stdio.h>
 
-/*int	ft_count_del(char const *s, char c)
-{
-	int	i;
-
-	i = 0;
-	while(*s++)
-	{
-		if(*s == c)
-			i++;
-	}
-	return(i);
-}
-
-char	**ft_split(char const *s, char c)
+/*char	**ft_split(char const *s, char c)
 {
 	char **buffer;
-	int	delim;
 	int	i;
 	int	j;
 
-	i = 0;
-	delim = ft_count_del(s, c);
-	while (*s || delim)
-	{
-		j = 0;
-		while (*s != c)
-		{
-			
-		}
-	}
+
 	return(buffer);
 }
 
--- libft.h
+++ libft.h
@@ -18,14 +18,41 @@
 
 int	ft_strlen(const char *str);
 int	ft_strncmp(const char *str1, const char *str2, size_t n);
+int	ft_atoi(const char *nptr);
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
+int	ft_isprint(int c);
+int	ft_memcmp(const void *str1, const void *str2, size_t n);
+int	ft_strncmp(const char *str1, const char *str2, size_t n);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
 
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, char *src, size_t size);
+size_t	ft_strlcat(char *dest, char *src, size_t size);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 
 void	*ft_calloc(size_t n, size_t size);
 void	ft_putchar_fd(char c, int fd);
+void	ft_bzero(void *str, size_t n);
+void	*ft_memchr(const void *str, int chr, size_t n);
+void	*ft_memset(void *str, int chr, size_t n);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putstr_fd(char *s, int fd);
 
 char	*ft_substr(char const *s, unsigned int start, size_t n);
 char	*ft_strdup(const char *s);
+char	*ft_itoa(int n);
+char	*ft_strchr(char *str, char chr);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strrchr(char *str, char chr);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_substr(char const *s, unsigned int start, size_t n);
+
+const char	*strnstr(const char *big, const char *little, size_t len);
 
 #endif
