2ed5661ff2bc8e57a10a603e5bc723edbaa78636
Cristian Bitca <cbitca@c2r2s6.42london.com>
2024-12-11 17:18:44 +0000

    split

--- ft_split.c
+++ ft_split.c
@@ -13,14 +13,51 @@
 #include "libft.h"
 #include <stdio.h>
 
+int	count_words(char const *s, char c)
+{
+	int	count;
+	int	i;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+			count++;
+		while (s[i] != c && s[i])
+			i++;
+		if (s[i] == c)
+			i++;
+	}
+	return (count);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	char **buffer;
-	int	i;
-	int	j;
+	char	**array;
+	int		i;
+	int		j;
+	int		a;
 
-
-	return(buffer);
+	i = 0;
+	j = 0;
+	a = 0;
+	array = ft_calloc(count_words(s, c) + 1, sizeof(char *));
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i] != c)
+			j = i;
+		while (s[i] != c && s[i])
+			i++;
+		array[a] = ft_substr(s, j, i - j);
+		a++;
+		while (s[i] == c)
+			i++;
+	}
+	array[a] = NULL;
+	return (array);
 }
 
 int	main(void)
