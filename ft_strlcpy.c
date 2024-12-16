f2d0cd6e842cbd3686f8a72ab09bcb1fe454029a
Cristian Bitca <cbitca@c2r4s9.42london.com>
2024-12-16 19:03:16 +0000

    strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -21,7 +21,7 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 	src_len = ft_strlen(src);
 	if (size != 0)
 	{
-		while (i < size - 1 && (src[i] || dest[i]))
+		while (i < (size - 1) && src[i])
 		{
 			dest[i] = src[i];
 			i++;
