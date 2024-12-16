738043e8df3fac823a77c85fa34696851c662fee
Cristian Bitca <cbitca@c2r4s9.42london.com>
2024-12-16 19:47:43 +0000

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -17,9 +17,9 @@ int	ft_strncmp(const char *str1, const char *str2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while ((str1[i] || str2[i]) && i < n)
+	while ((str1[i] || str2[i]) && i < n - 1)
 	{
-		if (str2[i] != str1[i])
+		if (str1[i] != str2[i])
 			return (str1[i] - str2[i]);
 		i++;
 	}
