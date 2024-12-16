e437583dfc6d3db231aaef204825fe262b65f356
Cristian Bitca <cbitca@c2r4s9.42london.com>
2024-12-16 20:38:08 +0000

    memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -17,12 +17,10 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	unsigned int	i;
 
 	i = 0;
-	if (n == 0)
-		return (0);
 	while (i != n)
 	{
-		if (*(unsigned char *)s1++ != *(unsigned char *)s2++)
-			return (*(unsigned char *)s1 - *(unsigned char *)s2);
+		if ((unsigned char *)s1[i] != (unsigned char *)s2[i])
+			return ((unsigned char *)s1[i] - (unsigned char *)s2[i]);
 		i++;
 	}
 	return (0);
