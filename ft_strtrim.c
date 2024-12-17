a81d598422bfa4a6a00a0cb9d385400506896762
Cristian Bitca <cbitca@c2r4s3.42london.com>
2024-12-17 18:18:38 +0000

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -37,13 +37,16 @@ char	*ft_strtrim(char const *s1, char const *set)
 	int		i;
 	int		j;
 
-	i = 0;
-	j = ft_strlen(s1) - 1;
-	while (find_set(set, s1[i]))
-		i++;
-	while (find_set(set, s1[j]))
-		j--;
-	buffer = ft_substr(s1, i, j);
+	if (s1 && set)
+	{
+		i = 0;
+		j = ft_strlen(s1) - 1;
+		while (find_set(set, s1[i]))
+			i++;
+		while (find_set(set, s1[j]))
+			j--;
+		buffer = ft_substr(s1, i, j);
+	}
 	return (buffer);
 }
 
