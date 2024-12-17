9c87f2b65524e9212374fbea402cb373fcdedc24
Cristian Bitca <cbitca@c2r4s3.42london.com>
2024-12-17 16:36:43 +0000

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -28,9 +28,3 @@ char	*ft_substr(char const *s, unsigned int start, size_t n)
 		ft_strlcpy(buffer, &s[start], n + 1);
 	return (buffer);
 }
-
-int	main(void)
-{
-	ft_substr("", 1, 1);
-	return (0);
-}
