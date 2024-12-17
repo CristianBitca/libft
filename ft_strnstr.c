46d668f6e578de02241fe5d26cc7564e038daede
Cristian Bitca <cbitca@c2r4s3.42london.com>
2024-12-17 22:22:37 +0000

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -18,17 +18,17 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	j;
 
 	i = 0;
-	if (*little == '\0' || little == NULL)
+	if (*little == '\0')
 		return ((char *)big);
-	while (big[i] || i < len)
+	while (big[i] && i < len)
 	{
+		j = 0;
 		while (big[i + j] == little[j])
 		{
 			j++;
 			if (little[j] == '\0')
 				return ((char *)&big[i]);
 		}
-		j = 0;
 		i++;
 	}
 	return (0);
