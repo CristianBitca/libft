5bb27c800d4158104590aa71ca76b16fb42322e1
Cristian Bitca <cbitca@c2r4s3.42london.com>
2024-12-17 20:55:19 +0000

    putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -17,13 +17,15 @@ void	ft_putnbr_fd(int n, int fd)
 	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
+		if (n <= -10)
+			ft_putnbr_fd((n / 10) * -1, fd);
 		ft_putchar_fd((n % 10) + '0', fd);
-		ft_putnbr_fd((n / 10) * -1, fd);
 	}
 	if (n >= 0)
 	{
 		ft_putchar_fd(n % 10 + '0', fd);
-		if (n > 9)
+		if (n >= 10)
 			ft_putnbr_fd(n / 10, fd);
 	}
 }
+
