5e3abec7fdf2623aee488ca3061608c6f53bb8cd
Cristian Bitca <cbitca@c2r4s3.42london.com>
2024-12-17 19:53:03 +0000

    fix

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -11,11 +11,11 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "ft_strlen.c"
+/*#include "ft_strlen.c"
 #include "ft_substr.c"
 #include "ft_strdup.c"
 #include "ft_calloc.c"
-#include "ft_strlcpy.c"
+#include "ft_strlcpy.c"*/
 
 int	find_set(char const *set, char c)
 {
@@ -55,10 +55,10 @@ char	*ft_strtrim(char const *s1, char const *set)
 	return (buffer);
 }
 
-int	main(void)
+/*int	main(void)
 {
 	char s1[] = "lorem ipsum dolor sit amet";
 
 	ft_strtrim(s1, "tel");
 	return (0);
-}
+}*/
