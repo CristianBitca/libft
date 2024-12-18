3dff0c158228d62ceeae9bb436f4c03b6860c168
Cristian Bitca <cbitca@c2r2s8.42london.com>
2024-12-18 19:39:24 +0000

    lstdel

--- libft.h
+++ libft.h
@@ -54,6 +54,8 @@ void	ft_putnbr_fd(int n, int fd);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_lstadd_front(t_list **lst, t_list *new);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
 
 char	*ft_substr(char const *s, unsigned int start, size_t n);
 char	*ft_strdup(const char *s);
