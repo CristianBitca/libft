ebb2bc45c31d056580afffedf1e08ee63e270bbf
CristianBitca <cristibitca2020@gmail.com>
2025-01-02 14:38:41 +0000

    lstmap

---
+++ ft_lstmap.c
@@ -0,0 +1,46 @@
+<<<<<<< HEAD
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbitca <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/18 20:13:32 by cbitca            #+#    #+#             */
+/*   Updated: 2024/12/18 20:13:33 by cbitca           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+=======
+>>>>>>> 327597c (bonus)
+
+#include "libft.h"
+
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*res;
+<<<<<<< HEAD
+
+	
+=======
+	t_list	*temp;
+	void	*content;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	res = 0;
+	while (lst)
+	{
+		content = f(lst->content);
+		temp = ft_lstnew(content);
+		if (!temp)
+		{
+			del(content);
+			ft_lstclear(&res, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&res, temp);
+		lst = lst->next;
+	}
+	return (res);
+>>>>>>> 327597c (bonus)
+}
