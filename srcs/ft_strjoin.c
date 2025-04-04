20c72370a8585606852261ba6ed47271e9e08748
Cristian Bitca <cbitca@c2r2s8.42london.com>
2025-04-04 21:35:26 +0100

    update

---
+++ srcs/ft_strjoin.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbitca <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/06 13:29:29 by cbitca            #+#    #+#             */
+/*   Updated: 2024/12/06 13:29:30 by cbitca           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*buffer;
+
+	buffer = ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 1, sizeof(char));
+	if (!buffer)
+		return (NULL);
+	ft_strlcpy(buffer, s1, ft_strlen(s1) + 1);
+	ft_strlcat(buffer, s2, ft_strlen(s1) + ft_strlen(s2) + 1);
+	return (buffer);
+}
