28d4944903661292bf1c7f17794395aa8a4e1bbb
Cristian Bitca <cbitca@c2r2s8.42london.com>
2025-04-07 14:35:34 +0100

    fix

--- libft/srcs/ft_strjoin.c
+++ libft/srcs/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: cbitca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 13:29:29 by cbitca            #+#    #+#             */
-/*   Updated: 2024/12/06 13:29:30 by cbitca           ###   ########.fr       */
+/*   Updated: 2025/04/07 14:35:17 by cbitca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*buffer;
-	int	len_s1;
-	int	len_s2;
+	int		len_s1;
+	int		len_s2;
 
 	len_s1 = ft_strlen(s1);
 	len_s2 = ft_strlen(s2);
